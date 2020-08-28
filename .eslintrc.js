b65eb3f2ad4352dd1f9eccdb9976837a8d7eb11c
Rogério Bispo <rogerio.bispo@nexaas.com>
2020-08-28 15:58:39 -0300

    basic settup for playing after

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "no-useless-constructor": "off",
+    "no-empty-function": "off",
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "max-classes-per-file": "off",
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off"
+  },
+};
